bb1ad1b24b0de2093cf3e2a7fba145e0146808a3
worksapce <289582304@qq.com>
2024-02-19 10:09:52 +0800

    update

---
+++ src/main/java/com/example/darwish/DarwishApplication.java
@@ -0,0 +1,13 @@
+package com.example.darwish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarwishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarwishApplication.class, args);
+	}
+
+}
